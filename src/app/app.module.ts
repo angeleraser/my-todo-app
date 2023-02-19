3348c680a6e63b97f03af37d6dd6bee1da84159d
Angel Figuera <angeleraser@gmail.com>
2023-02-19 09:04:58 -0400

    feat: add service worker module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,7 @@ import { SectionComponent } from './section/section.component';
 import { FormsModule } from '@angular/forms';
 import { AddTodoComponent } from './add-todo/add-todo.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,12 @@ import { TodoItemComponent } from './todo-item/todo-item.component';
     AddTodoComponent,
     TodoItemComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: !isDevMode(),
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
